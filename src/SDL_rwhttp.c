f2ad9c3dae8a6bbcaf3e352ccc3d47e5be0767b9
Martin Gerhardy <martin.gerhardy@gmail.com>
2013-07-18 15:50:56 +0200

    * curl may not be the only supported lib

--- src/SDL_rwhttp.c
+++ src/SDL_rwhttp.c
@@ -1,5 +1,13 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_CURL
 #include <curl/curl.h>
+#endif
 #include "SDL_rwhttp.h"
+
+#define STRINGIFY(x) #x
 
 int SDL_RWHttpShutdown (void)
 {
@@ -10,10 +18,14 @@ int SDL_RWHttpShutdown (void)
 int SDL_RWHttpInit (void)
 {
 	SDL_SetHint(SDL_RWHTTP_HINT_USER_AGENT, "sdl_rwhttp/1.0");
+#ifdef HAVE_CURL
 	const CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
 	if (result == CURLE_OK)
 		return 0;
 	SDL_SetError(curl_easy_strerror(result));
+#else
+	SDL_SetError("Required library is missing");
+#endif
 	return -1;
 }
 
@@ -32,8 +44,10 @@ static int SDLCALL http_close (SDL_RWops * context)
 		return status;
 
 	if (context->hidden.unknown.data1) {
+#ifdef HAVE_CURL
 		CURL *curl = http_get_curl(context);
 		curl_easy_cleanup(curl);
+#endif
 	}
 	if (context->hidden.unknown.data2) {
 		http_data_t *data = (http_data_t*) context->hidden.unknown.data2;
@@ -44,7 +58,8 @@ static int SDLCALL http_close (SDL_RWops * context)
 	return status;
 }
 
-static size_t httpWriteSync (void *streamData, size_t size, size_t nmemb, void *userData)
+#ifdef HAVE_CURL
+static size_t curlHttpWriteSync (void *streamData, size_t size, size_t nmemb, void *userData)
 {
 	const size_t realsize = size * nmemb;
 	http_data_t *httpData = (http_data_t *) userData;
@@ -61,20 +76,24 @@ static size_t httpWriteSync (void *streamData, size_t size, size_t nmemb, void *
 
 	return realsize;
 }
+#endif
 
 SDL_RWops* SDL_RWFromHttpSync (const char *uri)
 {
 	SDL_RWops *rwops;
-	CURL* curlHandle;
 	http_data_t *httpData;
+#ifdef HAVE_CURL
+	CURL* curlHandle;
 	CURLcode result;
+#endif
 
 	httpData = SDL_malloc(sizeof(*httpData));
 	SDL_zerop(httpData);
 
+#ifdef HAVE_CURL
 	curlHandle = curl_easy_init();
 	curl_easy_setopt(curlHandle, CURLOPT_URL, uri);
-	curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, httpWriteSync);
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, curlHttpWriteSync);
 	curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void * )httpData);
 	curl_easy_setopt(curlHandle, CURLOPT_USERAGENT, SDL_GetHint(SDL_RWHTTP_HINT_USER_AGENT));
 
@@ -83,6 +102,7 @@ SDL_RWops* SDL_RWFromHttpSync (const char *uri)
 		SDL_SetError(curl_easy_strerror(result));
 		return NULL ;
 	}
+#endif
 
 	rwops = SDL_RWFromConstMem(httpData->data, httpData->size);
 	rwops->hidden.unknown.data1 = curlHandle;
